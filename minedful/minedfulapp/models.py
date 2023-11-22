d1cfa274e8f2222da431d00f8e4edb8aa8dbe37b
sayaring <115022654+sayaring@users.noreply.github.com>
2023-11-22 23:18:51 +0530

    Update models.py

--- minedful/minedfulapp/models.py
+++ minedful/minedfulapp/models.py
@@ -1,20 +1,15 @@
 from django.db import models
 from djongo import models
 
-class Activity(models.Model):
-    name = models.CharField()
-    frequency = models.IntegerField()
 
 class Report(models.Model):
     user_id = models.AutoField(primary_key= True)
     password = models.CharField(max_length= 50)
     variants = models.IntegerField()
     logs = models.IntegerField()
-    case_arrival = models.DecimalField()
-    case_duration = models.DecimalField()
-    trace_distribution = models.CharField()
-    flowchart = models.CharField()
-    opening_activities = models.ArrayField(model_container = Activity)
-    ending_activities = models.ArrayField(model_container = Activity)
-    rework_activities = models.ArrayField(model_container = Activity)
+    case_arrival = models.DecimalField(max_digits=10, decimal_places=2)
+    case_duration = models.DecimalField(max_digits=10, decimal_places=2)
+    opening_activities = models.JSONField()
+    ending_activities = models.JSONField()
+    rework_activities = models.JSONField()
     
