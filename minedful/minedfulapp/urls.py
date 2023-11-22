9882384807ae3e72050db2f2ff5955560759383e
sayaring <115022654+sayaring@users.noreply.github.com>
2023-11-23 04:04:19 +0530

    Update urls.py

--- minedful/minedfulapp/urls.py
+++ minedful/minedfulapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('report/', views.upload_file, name='upload_file'),
+    #path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/signup/', views.signup, name='signup'),
+    path('api/login/', views.user_login, name='user_login'),
+    path('upload/', views.upload_file, name='upload_file'),
 ]
